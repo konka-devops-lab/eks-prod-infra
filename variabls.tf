fe53508404e26c109ccd135aee33c0bd68d31707
siva <sivaram0434@gmail.com>
2025-06-22 15:26:42 +0530

    remove the type of variable

--- variabls.tf
+++ variabls.tf
@@ -3,10 +3,7 @@ variable "aws_region" {
   type        = string
 }
 variable "common_variables" {
-  description = "Common variables for the infrastructure"
-  type = map(any)
+
 }
 variable "vpc" {
-  description = "VPC configuration variables"
-  type = map(any)
 }
